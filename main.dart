cbb5695d28fef048a8f3fd3c7400fe1f7be88472
MohammedManik2548 <75543975+MohammedManik2548@users.noreply.github.com>
2021-08-01 11:54:41 +0600

    practice polymorphism

---
+++ main.dart
@@ -0,0 +1,36 @@
+void main(List<String> args) {
+  Teacher teacher = Teacher("Jahangir", "Msc");
+  teacher.teaching();
+
+  Student student = Student("Manik", "Bsc", 07);
+  student.study();
+}
+
+class Person {
+  final String name;
+  Person(this.name);
+
+  Name() {
+    print("My name is Manik");
+  }
+}
+
+class Teacher extends Person {
+  final String degree;
+
+  Teacher(name, this.degree) : super(name);
+
+  teaching() {
+    print("Teaching student");
+  }
+}
+
+class Student extends Teacher {
+  final int id;
+
+  Student(name, var degree, this.id) : super(name, degree);
+
+  study() {
+    print("Student study");
+  }
+}
